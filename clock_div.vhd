518d6e06e6a17182a69fac8723df7fed2ed3657c
ndiocson <ndiocson@outlook.com>
2019-09-23 14:23:01 -0600

    more code

--- clock_div.vhd
+++ clock_div.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity clock_div is
     Generic (
@@ -16,6 +17,28 @@ end entity clock_div;
 
 architecture divide of clock_div is
 
+-- count:           Signal to keep track of current count
+-- clk_period:      Period for on-board clock frequency
+-- clk_out_period:  Period for new clock, given frequency
+-- max_count:       Number of cucles in on-board clock to represent one new clock cycle
+signal count            : std_logic_vector(31 downto 0) := (others => '0');
+constant clk_period     : integer := to_integer(1 / CLK_FREQ);
+constant clk_out_period : integer := to_integer((1 / to_integer(unsigned(freq))));
+constant max_count      : std_logic_vector(31 downto 0) := std_logic_vector(unsigned(to_integer(clk_out_period / clk_period)));
+
 begin
+    
+    -- Process to count number of cycles from on-board clock
+    -- needed to generate new clock of given frequency
+    count_proc: process(clk) is
+        if (rising_edge(clk)) then
+            if (count >= max_count or reset = '1') then
+                count <= (others => '0');
+                clk_out <= not clk_out;
+            else
+                count <= count + 1;
+            end if;
+        end if;
+    end process count_proc;
     
 end architecture divide;
