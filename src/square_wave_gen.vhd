bcce223f67ce082ab630593b8a8cad689b2de75a
ndiocson <ndiocson@outlook.com>
2019-09-24 06:41:55 -0600

    rearranged internal signals

--- src/square_wave_gen.vhd
+++ src/square_wave_gen.vhd
@@ -32,11 +32,11 @@ end Square_Wave_Gen;
 
 architecture Behavioral of Square_Wave_Gen is
 
+-- wave_freq:           Frequency of desired square wave 
+constant wave_freq      : integer := to_integer(unsigned(freq));
+
 -- new_clk:             Internal std_logic signal used to drive out_wave
 signal new_clk          : std_logic := '0';
-
--- wave_freq:           
-constant wave_freq      : integer := to_integer(unsigned(freq));
 
 begin
 
