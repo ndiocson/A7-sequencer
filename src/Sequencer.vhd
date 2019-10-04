170633ccc37a5fdb7ff00202a0363f996fda9c8f
ndiocson <ndiocson@outlook.com>
2019-10-03 22:05:02 -0600

    added components for Clock_Divider and Square_Wave_Gen

--- src/Sequencer.vhd
+++ src/Sequencer.vhd
@@ -41,34 +41,61 @@ end entity Sequencer;
 
 architecture Behavioral of Sequencer is
 
--- new_clk:             Internal std_logic signal used as system clock      
--- curr_step:           Internal step signal used to track position in sequencer
--- step_note:           Internal signal array of indicies representing each step wave
--- note_freq:           Internal signal array of frequencies corresponding to each step          
-signal new_clk          : std_logic;
-signal curr_step        : step := 0;
-signal step_wave        : step_arr := (others => '0');
-signal note_freq        : freq_arr := (others => (others => '0'));
+-- Clock_Divider Component Declaration
+component Clock_Divider is
+    Generic (
+            CLK_FREQ        : positive := 1E7;      -- on-board clock frequency (10 MHz)
+            CLK_OUT_FREQ    : positive := 2         -- desired clock frequency (default 2 Hz)
+            );
+    Port (
+            clk, reset      : in std_logic;
+            clk_out         : out std_logic
+            );
+end component Clock_Divider;
+
+-- Square_Wave_Gen Component Declaration
+component Square_Wave_Gen is
+    Generic (
+            CLK_FREQ        : positive := 1E7;                  -- on-board clock frequency (10 MHz)
+            FREQ_BITS       : positive := 32
+            );
+    Port ( 
+            clk, reset      : in std_logic;
+            freq            : in std_logic_vector(FREQ_BITS - 1 downto 0);
+            out_wave        : out std_logic
+            );
+end component Square_Wave_Gen;
+
+-- new_clk:         Internal std_logic signal used as system clock      
+-- curr_step:       Internal step signal used to track position in sequencer
+-- step_note:       Internal signal array of indicies representing each step wave
+-- note_freq:       Internal signal array of frequencies corresponding to each step          
+signal new_clk      : std_logic;
+signal curr_step    : step := 0;
+signal step_wave    : step_arr := (others => '0');
+signal note_freq    : freq_arr := (others => (others => '0'));
 
 begin
     
     -- Instantiates Clock_Divider with default actual generic values
     new_clock: entity work.Clock_Divider(Behavioral)
                 Generic Map (CLK_FREQ => open, CLK_OUT_FREQ => open)
-                Port Map(clk => clk, reset => reset, clk_out => new_clk);
+                Port Map (clk => clk, reset => reset, clk_out => new_clk);
                 
     -- Instatiates N_STEPS Square_Wave_Gen models for each step within the sequencer
     -- TODO: implement a fix for steps of 0 Hz frequency values (should act as rests)
     generate_waves: for index in 0 to N_STEPS - 1 generate
         square_wave: entity work.Square_Wave_Gen(Behavioral)
-                Port Map(clk => clk, reset => reset, freq => note_freq(index), out_wave => step_wave(index));
+                Generic Map (CLK_FREQ => open, FREQ_WIDTH => open)
+                Port Map (clk => clk, reset => reset, freq => note_freq(index), out_wave => step_wave(index));
     end generate generate_waves;
 
     -- Concurrently assigns note frequency values to each element in steps array
+    -- If the no note is assigned, the note is considered as a 'rest'
     -- TODO: note frequency values to be determined by buttons on fpga; hard-coded to 220 Hz for now    
     note_assign: for index in note_freq'range generate
         note_freq(index) <= std_logic_vector(to_unsigned(220, note_freq(index)'length))
-                        when rising_edge(new_clk) else (others => '0');
+                        when rising_edge(new_clk) else (others => '1');
     end generate note_assign;
     
     -- Uses the curr_step signal index to apply square wave of corresponding note frequency driven by Sqaure_Wave_Gen
